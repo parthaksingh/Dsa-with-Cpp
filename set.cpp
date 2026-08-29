a3906624bf15c8a7c695bfedc825257c3c9b02d8
Parthak <parrthaksingh@parthaks-MacBook-Air.local>
2026-08-29 23:47:05 +0530

    Added solution to count all distinct possible exam scores using an unordered_set.

--- set.cpp
+++ set.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include<set>
+#include<unordered_set>
 #include <vector>
 
 using namespace std;
@@ -97,28 +98,67 @@ using namespace std;
 
 
 //fine the second smallest value from vector;
-int main(){
+// int main(){
 
-    int n;
-    cin>>n;
+//     int n;
+//     cin>>n;
     
-    vector<int> v1(n);
+//     vector<int> v1(n);
 
-    cout<<"vector elements: ";
-    for(int i = 0; i<n ; i++){
-        cin>>v1[i];
+//     cout<<"vector elements: ";
+//     for(int i = 0; i<n ; i++){
+//         cin>>v1[i];
+//     }
+//     cout<<endl;
+
+//     set<int> s1;
+//     for(auto x:v1){
+//         s1.insert(x);
+//     }
+
+//     auto it = s1.begin();
+//     //advance(it, 1);  //anyone can you use;
+//     it++;
+//     cout<<"Second smallest value: "<<*it<<endl;
+
+//     return 0;
+// }
+
+
+// Given the number of questions as n, and marks for the correct answer as p and q marks for the incorrect answer.
+
+// For each question, you can:
+
+// * Attempt it correctly → get p marks
+// * Attempt it incorrectly → get q marks
+// * Leave it unanswered → get 0 marks
+
+// Find the number of all different possible marks that one can score in the examination.
+
+int main(){
+    int n, p, q;
+    cin>>n>>p>>q;
+
+    unordered_set<int> s1;
+    
+    for(int i = 0; i <= n; i++){
+        for(int j = 0; j <=n; j++){
+            int currect = i;
+            int incurrect = j;
+            int untuach = n - (i + j);
+
+            if(untuach >= 0){
+                int score = currect*p + incurrect*q;
+                s1.insert(score);
+            }else{
+                break;
+            }
+        }
+    }
+    for(auto x:s1){
+        cout<<x<<" ";
     }
     cout<<endl;
-
-    set<int> s1;
-    for(auto x:v1){
-        s1.insert(x);
-    }
-
-    auto it = s1.begin();
-    //advance(it, 1);  //anyone can you use;
-    it++;
-    cout<<"Second smallest value: "<<*it<<endl;
-
+    cout<<s1.size()<<endl;
     return 0;
 }
