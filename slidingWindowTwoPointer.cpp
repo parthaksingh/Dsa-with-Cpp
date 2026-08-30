8151ff8c0538a7896e6428fdae318a2119c341c1
Parthak <parrthaksingh@parthaks-MacBook-Air.local>
2026-08-30 17:56:10 +0530

    Implement maximum sum using sliding window

--- slidingWindowTwoPointer.cpp
+++ slidingWindowTwoPointer.cpp
@@ -5,31 +5,60 @@ using namespace std;
 
 //Implement maximum sum from both ends
 
-int findMaxSum(vector<int>& nums, int k){
+// int findMaxSum(vector<int>& nums, int k){
     
-    int n = nums.size(); 
-    int rSum = 0, lSum = 0, maxSum = 0;
+//     int n = nums.size(); 
+//     int rSum = 0, lSum = 0, maxSum = 0;
 
-    for(int i =0; i<k; i++){
-        lSum+=nums[i];
+//     for(int i =0; i<k; i++){
+//         lSum+=nums[i];
+//     }
+//     maxSum = lSum;
+
+//     int Rindex = n - 1;
+//     for(int i = k - 1; i>=0; i--){
+//         lSum-= nums[i];
+//         rSum+=nums[Rindex];
+//         Rindex--;
+//         maxSum = max(maxSum, lSum+ rSum);
+//     }
+
+//     return maxSum;
+// }
+
+// int main(){
+//     vector<int> v1 = {6,2,3,4,7,1,7,1};
+//     int key = 4;
+
+//     cout<<findMaxSum(v1, key)<<endl;
+//     return 0;
+// }
+
+
+// Implement maximum sum using sliding window
+
+int findMaxSum(vector<int>& nums, int k){
+    int n = nums.size();
+    int sum = 0, maxSum = 0;
+    
+    for(int i = 0; i<k; i++){
+        sum+=nums[i];
     }
-    maxSum = lSum;
+    maxSum = sum;
 
-    int Rindex = n - 1;
-    for(int i = k - 1; i>=0; i--){
-        lSum-= nums[i];
-        rSum+=nums[Rindex];
-        Rindex--;
-        maxSum = max(maxSum, lSum+ rSum);
+    for(int i = k; i < n; i++){
+        sum+=nums[i];
+        sum-=nums[i - k];
+
+        maxSum = max(sum, maxSum);
     }
-
     return maxSum;
 }
 
 int main(){
-    vector<int> v1 = {6,2,3,4,7,1,7,1};
-    int key = 4;
+    vector<int> v1 = {6,2,3,4,8,7,4,1,4};
+    int key = 3;
 
-    cout<<findMaxSum(v1, key)<<endl;
+    cout<<findMaxSum(v1, key);
     return 0;
 }
