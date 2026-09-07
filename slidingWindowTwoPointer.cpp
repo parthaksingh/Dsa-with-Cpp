b74e91d78bca04fcb290c2a62b368d02e0f54334
Parthak <parrthaksingh@parthaks-MacBook-Air.local>
2026-09-07 22:09:37 +0530

    add substring counting for abc bruth force

--- slidingWindowTwoPointer.cpp
+++ slidingWindowTwoPointer.cpp
@@ -363,16 +363,32 @@ using namespace std;
 // }
 
 //Add substring counting for abc
+// int fun(string s){
+//     int n = s.size();
+//     int count = 0;
+//     int lastseen[3] = {-1, -1, -1};
+    
+//     for(int i = 0; i < n; i++){
+
+//         lastseen[s[i] - 'a'] = i;
+//         if(lastseen[0] != -1 && lastseen[1] != -1 && lastseen[2] != -1){
+//             count = count + 1 + min(lastseen[0], min(lastseen[1], lastseen[2]));
+//         }
+//     }
+//     return count;
+// }
 int fun(string s){
     int n = s.size();
     int count = 0;
-    int lastseen[3] = {-1, -1, -1};
-    
-    for(int i = 0; i < n; i++){
 
-        lastseen[s[i] - 'a'] = i;
-        if(lastseen[0] != -1 && lastseen[1] != -1 && lastseen[2] != -1){
-            count = count + 1 + min(lastseen[0], min(lastseen[1], lastseen[2]));
+    for(int i = 0; i < n; i++){
+        int hash[3] = {0};
+        for(int j = i; j < n; j++){
+            hash[s[j] - 'a'] = 1;
+
+            if(hash[0] + hash[1]+ hash[2] == 3){
+                count += 1;
+            }
         }
     }
     return count;
