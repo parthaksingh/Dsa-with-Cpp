7547665b0091924dba7444a1c16b14faa1c97a1f
Parthak <parrthaksingh@parthaks-MacBook-Air.local>
2026-09-12 15:49:46 +0530

    Implement linked list node creation

---
+++ linkedlist.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+//Implement linked list node creation
+class Node {
+public:
+    int data;
+    Node* next;
+
+    Node(int data1, Node* next1) {
+        data = data1;
+        next = next1;
+    }
+    Node(int data1){
+        data = data1;
+        next = nullptr;
+    }
+};
+
+int main(){
+    vector<int> ans = {2, 5, 8, 7};
+    Node* head = new Node(ans[0]);
+
+    Node* second = new Node(ans[1]);
+    head->next = second;
+    Node* third = new Node(ans[2]);
+    second->next = third;
+    Node* fourth = new Node(ans[3]);
+    third->next = fourth;
+
+    Node* temp = head;
+    while(temp != nullptr){
+        cout<<temp->data<<" ";
+        temp = temp->next;
+    }
+    cout<<endl;
+
+    return 0;
+}
